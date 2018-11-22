79c45f1703137f08234fb82d7510339248fe595c
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2018-11-22 12:16:18 -0800

    Queue via Stack

---
+++ CCI-Solutions/src/chapter3/QueueViaStack.java
@@ -0,0 +1,40 @@
+package chapter3;
+import java.util.Stack;
+
+// Queues via Stack
+
+public class QueueViaStack {
+    Stack<Integer> stackNewest, stackOldest;
+
+    public QueueViaStack(){
+        stackNewest = new Stack<Integer>();
+        stackOldest = new Stack<Integer>();
+    }
+
+    public int size(){
+        return stackNewest.size() + stackOldest.size();
+    }
+
+    public void add(int value){
+        stackNewest.push(value);
+    }
+
+    private void shiftOfStack(){
+        if(stackOldest.isEmpty()){
+            while(!stackNewest.isEmpty()){
+                stackOldest.push(stackNewest.pop());
+            }
+        }
+    }
+
+    public int remove(){
+        shiftOfStack();
+        return stackOldest.pop();
+    }
+
+    public int peek(){
+        shiftOfStack();
+        return stackOldest.peek();
+    }
+}
+
