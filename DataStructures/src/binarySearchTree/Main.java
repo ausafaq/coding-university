69c8c04555ca42cfb199a772c80e902894a83700
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2018-11-22 23:18:34 -0800

    Tree Impl

--- DataStructures/src/binarySearchTree/Main.java
+++ DataStructures/src/binarySearchTree/Main.java
@@ -3,27 +3,14 @@ package binarySearchTree;
 public class Main {
 
     public static void main(String[] args){
+        Tree myTree = new Tree();
+        myTree.insert(4);
+        myTree.insert(2);
+        myTree.insert(5);
+        myTree.insert(1);
+        myTree.insert(3);
+        //myTree.traversePostOrder();
 
-        Tree intTree  = new Tree();
-        intTree.insert(25);
-        intTree.insert(20);
-        intTree.insert(15);
-        intTree.insert(27);
-        intTree.insert(30);
-        intTree.insert(29);
-        intTree.insert(26);
-        intTree.insert(22);
-        intTree.insert(32);
-
-        /*System.out.print("\n InOrder Traversal -> ");
-        intTree.traverseInOrder();
-
-        System.out.print("\n PreOrder Traversal -> ");
-        intTree.traversePreOrder();
-
-        System.out.print("\n PostOrder Traversal -> ");
-        intTree.traverserPostOrder();*/
-
-        System.out.println(intTree.max());
+        System.out.println(myTree.hasPathSum(9));
     }
 }
