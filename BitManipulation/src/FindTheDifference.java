e0a054ab6c0c8084aab22457fce4fae25bb97621
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2018-12-09 23:47:33 -0800

    Find the difference

---
+++ BitManipulation/src/FindTheDifference.java
@@ -0,0 +1,16 @@
+public class FindTheDifference {
+
+    public static char findTheDifference(String s, String t) {
+        int n = t.length();
+        char c = t.charAt(n - 1);
+        for (int i =0 ; i < n - 1; i++) {
+            c ^= s.charAt(i);
+            c ^= t.charAt(i);
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findTheDifference("abcd", "abcde"));
+    }
+}
