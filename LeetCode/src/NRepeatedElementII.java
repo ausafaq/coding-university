578109d4628c54aee4286c4ba53a9a16d65f6606
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2018-12-26 23:08:13 -0800

    N-repeated Element II

---
+++ LeetCode/src/NRepeatedElementII.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class NRepeatedElementII {
+
+    public static int repeatedNTimes(int[] A) {
+        int n = A.length / 2;
+        int num = 0;
+        Map<Integer, Integer> intCount = new HashMap<Integer, Integer>();
+        for(int i : A) {
+            if(intCount.containsKey(i)) {
+                intCount.put(i, intCount.get(i) + 1);
+            } else {
+                intCount.put(i, 1);
+            }
+        }
+
+        for(Map.Entry hm  : intCount.entrySet() ) {
+            if((int)hm.getValue() == n) {
+                num = (int) hm.getKey();
+            }
+        }
+        return num;
+    }
+
+
+    public static void main(String[] args) {
+        int[] input = {2, 1, 2, 5, 3, 2};
+        System.out.println(repeatedNTimes(input));
+    }
+}
+
