7063a9d4aff3af7d3e3cd2945293692af7d947c2
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2018-12-30 07:03:05 -0800

    Add Digits

---
+++ LeetCode/src/Question258.java
@@ -0,0 +1,21 @@
+public class Question258 {
+
+    public static int addDigits(int num) {
+
+        if(num < 10) {
+            return num;
+        } else {
+            int sum = 0;
+            while(num != 0) {
+                sum = sum + num%10;
+                num = num/10;
+            }
+            num = sum;
+        }
+        return addDigits(num);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addDigits(38));
+    }
+}
