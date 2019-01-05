8ef32782960dc2026630693de1015230b28b4e09
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-01-05 01:09:31 -0800

    Reshape Matrix

---
+++ LeetCode/src/Question566.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Question566 {
+
+    public static int[][] matrixReshape(int[][] nums, int r, int c) {
+
+        int[][] result = new int[r][c];
+        int rows = nums.length;
+        int cols = nums[0].length;
+
+        Queue<Integer> matQueue = new LinkedList<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matQueue.add(nums[i][j]);
+            }
+        }
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                result[i][j] = matQueue.remove();
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] input = {{1, 2}, {3, 4}};
+    }
+}
