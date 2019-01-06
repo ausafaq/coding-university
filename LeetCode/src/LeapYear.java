ad113ec15e6de183c9eaba6a4da8c68bd07d8dbc
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-01-05 18:53:26 -0800

    Check leap year

---
+++ LeetCode/src/LeapYear.java
@@ -0,0 +1,26 @@
+public class LeapYear {
+
+    public static boolean isCheckYear(int year) {
+
+        // If a year is a multiple of 400
+        // then it is a leap year
+        if(year % 400 == 0)
+            return true;
+
+        // Else if a year is divisible by 100
+        // then not a leap year
+        else if(year % 100 == 0)
+            return false;
+
+        // Else if divisible by 4
+        // then a leap year
+        else if(year % 4 == 0)
+            return true;
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isCheckYear(2016));
+    }
+}
