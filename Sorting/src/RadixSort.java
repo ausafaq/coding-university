bc3831eba412cce36ab2b72cc84f6cdc3992e981
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-01-11 10:36:16 -0800

    Radix Sort

--- Sorting/src/RadixSort.java
+++ Sorting/src/RadixSort.java
@@ -3,8 +3,8 @@ public class RadixSort {
     public static int getMax(int[] input) {
         int max = input[0];
 
-        for(int i = 1; i < input.length; i++) {
-            if(input[i] > max)
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] > max)
                 max = input[i];
         }
 
@@ -18,25 +18,25 @@ public class RadixSort {
         int[] result = new int[n];
         int largestNum = getMax(input);
 
-        while(largestNum/digitPlace > 0) {
+        while (largestNum / digitPlace > 0) {
             int[] count = new int[10];
 
-            for(int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++) {
                 count[input[i] / digitPlace % 10]++;
             }
 
             // Adjusting the count
-            for(int i = 1; i < 10; i++) {
-                count[i] += count[i-1];
+            for (int i = 1; i < 10; i++) {
+                count[i] += count[i - 1];
             }
 
             // Build the resulting array
-            for(int i = n - 1; i >= 0; i--) {
+            for (int i = n - 1; i >= 0; i--) {
                 result[count[input[i] / digitPlace % 10] - 1] = input[i];
                 count[input[i] / digitPlace % 10]--;
             }
 
-            for(int i = 0; i < result.length; i++) {
+            for (int i = 0; i < result.length; i++) {
                 input[i] = result[i];
             }
 
@@ -45,13 +45,12 @@ public class RadixSort {
     }
 
 
-
     public static void main(String[] args) {
         int[] radixArray = {4725, 4586, 1330, 8792, 1594, 5729};
 
         radixSort(radixArray);
 
-        for(int num : radixArray) {
+        for (int num : radixArray) {
             System.out.print(num + " ");
         }
     }
