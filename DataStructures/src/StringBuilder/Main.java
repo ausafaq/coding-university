c405464b5194bb6fd6bad2f28c5660cc02394fb9
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-03-21 00:52:02 -0700

    StringBuilder Impl

---
+++ DataStructures/src/StringBuilder/Main.java
@@ -0,0 +1,12 @@
+package StringBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Ausaf");
+        System.out.println(sb);
+        sb.append(" Ahmed");
+        System.out.println(sb);
+    }
+}
---
+++ DataStructures/src/StringBuilder/StringBuilder.java
@@ -0,0 +1,31 @@
+package StringBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringBuilder {
+
+    private List<Character> characterBuffer;
+
+    public StringBuilder() {
+        characterBuffer = new ArrayList<Character>();
+    }
+
+    public void append(String anotherString) {
+        if(anotherString != null) {
+            char[] charArray = anotherString.toCharArray();
+            for(int i=0;i<charArray.length; i++) {
+                characterBuffer.add(charArray[i]);
+            }
+        }
+    }
+
+    public String toString() {
+        char[] charArray = new char[characterBuffer.size()];
+        for(int i=0; i<charArray.length; i++) {
+            charArray[i] = characterBuffer.get(i);
+        }
+        return new String(charArray);
+    }
+
+}
