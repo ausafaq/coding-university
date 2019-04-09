b539e19e1f197f874054e3e4c7db1f2e3e3b5ed8
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-04-08 23:52:24 -0700

    Level Order Traversal

---
+++ LeetCode/src/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+
+public class BinaryTreeLevelOrderTraversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    class Solution {
+
+        public List<List<Integer>> levelOrder(TreeNode root) {
+
+            if(root == null) {
+                return Collections.emptyList();
+            }
+
+            List<List<Integer>> result = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+
+            queue.offer(root);
+
+            while(!queue.isEmpty()) {
+                int levelSize = queue.size();
+                List<Integer> current = new ArrayList<>();
+
+                for(int i = 0; i < levelSize; i++) {
+                    TreeNode currentNode = queue.poll();
+                    current.add(currentNode.val);
+
+                    TreeNode left = currentNode.left;
+                    if(left != null) {
+                        queue.offer(left);
+                    }
+
+                    TreeNode right = currentNode.right;
+                    if(right != null) {
+                        queue.offer(right);
+                    }
+                }
+                result.add(current);
+            }
+            return result;
+        }
+    }
+}
