fa636dd87c2bb77a2e93f1b19b5e6f875ed2fc66
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-06-10 23:37:50 -0700

    Valid Parentheses

---
+++ LeetCode/src/ValidParentheses.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for (char c : s.toCharArray()){
+            if (c == '(' || c == '[' || c == '{')
+                stack.push(c);
+            else if (c == ')'){
+                if (stack.isEmpty() || stack.pop() != '(')return false;
+            }else if (c == ']'){
+                if (stack.isEmpty() || stack.pop() != '[')return false;
+            }else if (c == '}'){
+                if (stack.isEmpty() || stack.pop() != '{')return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String input = "()[]}";
+        System.out.println(isValid(input));
+    }
+}
