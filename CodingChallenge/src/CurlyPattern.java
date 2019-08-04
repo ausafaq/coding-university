71551db598271a9e43891e8fc3a556f2a98d6392
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-08-04 00:29:54 -0700

    Curly Pattern

--- CodingChallenge/src/CurlyPattern.java
+++ CodingChallenge/src/CurlyPattern.java
@@ -3,23 +3,16 @@ import java.util.Scanner;
 public class CurlyPattern {
 
     public static void curlyPattern(int n) {
-        for(int i = 1; i <= n; i++) {
-            printLeft(i);
-            printRight(i);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print('{');
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print('}');
+            }
         }
 
-    }
-
-    public static void printLeft(int n) {
-        for(int i = 1; i <= n; i++) {
-            System.out.print('{');
-        }
-    }
-
-    public static void printRight(int n) {
-        for(int i = 1; i <= n; i++) {
-            System.out.print('}');
-        }
     }
 
     public static void main(String[] args) {
