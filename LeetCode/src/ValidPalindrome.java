df02cdaad0cb0133e122eede67627999b7163811
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-10-06 11:37:27 -0700

    LeetCode Valid Palindrome

--- LeetCode/src/ValidPalindrome.java
+++ LeetCode/src/ValidPalindrome.java
@@ -12,8 +12,30 @@ public class ValidPalindrome {
         return true;
     }
 
+    public static boolean isPalindromeII(String s) {
+        char[] arr = s.toLowerCase().toCharArray();
+        int i = 0;
+        int j = arr.length - 1;
+
+        while(i < j) {
+            while((i <= arr.length - 1) && !((arr[i] >='a' && arr[i] <= 'z') || (arr[i]>='0'&& arr[i]<='9'))) {
+                i++;
+            }
+
+            while((j >= 0) && !((arr[j] >='a' && arr[j] <= 'z') || (arr[i]>='0'&& arr[i]<='9'))) {
+                j--;
+            }
+
+            if(arr[i++] != arr[j--]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println(isPalindrome("manam"));
+        System.out.println(isPalindromeII("race a car"));
     }
 
 }
