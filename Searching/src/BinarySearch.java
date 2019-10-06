ebcbac8257ad6c53268e51c859b95ce8f9e89ac6
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-10-06 13:04:25 -0700

    Binary search Impl

--- Searching/src/BinarySearch.java
+++ Searching/src/BinarySearch.java
@@ -1,25 +1,45 @@
 public class BinarySearch {
 
-    public static int binarySearch(int[] input, int start, int end, int element) {
-        int mid = 0;
-       while(start <= end) {
-           mid = (start +  end) / 2;
-           if(input[mid] == element) {
-               return mid;
-           } else if(input[mid] < element) {
-               start = mid + 1;
-           } else {
-               end = mid - 1;
-           }
-       }
-       return mid;
+    public static int binarySearchIter(int[] input, int target, int low, int high) {
+        int index = -1;
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+
+            if(target == input[mid]) {
+                index = mid;
+                break;
+            } else if (target < input[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return index;
+    }
+
+    public static int binarySearchRecur(int[] input, int target, int low, int high) {
+
+        int mid = (low + high) / 2;
+
+        if(high < low) {
+            return -1;
+        }
+
+        if(target == input[mid]) {
+            return mid;
+        } else if (target < input[mid]) {
+            return binarySearchRecur(input, target, low, mid - 1);
+        } else {
+            return binarySearchRecur(input, target, mid + 1, high);
+        }
     }
 
     public static void main(String[] args) {
 
-        int[] input = {-22, 1, -15, 7, 20, 35, 55};
+        int[] input = {-22, -15, 1, 7, 20, 35, 55};
 
-        System.out.println(binarySearch(input, 0, input.length, -16));
-
+        System.out.println(binarySearchIter(input, -105, 0, input.length - 1));
+        System.out.println(binarySearchRecur(input, -105, 0, input.length - 1));
     }
 }
