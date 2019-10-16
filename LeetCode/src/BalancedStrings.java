0eb4a096935649c529b68ba0b52240199b3d68b1
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-10-15 23:28:25 -0700

    Balanced String Checker II

--- LeetCode/src/BalancedStrings.java
+++ LeetCode/src/BalancedStrings.java
@@ -15,7 +15,27 @@ public class BalancedStrings {
         return count;
     }
 
+    public static  int balancedStringSplitCheck(String s) {
+        // RLRRLLRLRL
+        int count = 0;
+        int R = 0, L = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'R') {
+                R++;
+            } else if (s.charAt(i) == 'L') {
+                L++;
+            }
+            if(L == R) {
+                count++;
+                R = 0;
+                L = 0;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-        System.out.println(balancedStringSplit("RLRRLLRLRL"));
+        System.out.println(balancedStringSplitCheck("RLRRLLRLRL"));
     }
 }
