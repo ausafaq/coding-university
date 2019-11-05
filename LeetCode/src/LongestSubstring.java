6cc0707fc49d9fe84062592146c288baff5c7699
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-11-04 18:25:03 -0800

    Longest Palindromic Substring

--- LeetCode/src/LongestSubstring.java
+++ LeetCode/src/LongestSubstring.java
@@ -1,33 +1,6 @@
 import java.util.*;
 
 public class LongestSubstring {
-    /* Brute-force solution
-       O(n3)
-     */
-//    public static int lengthOfLongestSubstring(String str) {
-//        int len = str.length();
-//        int result = 0;
-//        for(int i = 0; i < len; i++) {
-//            for(int j = i + 1; j <= len; j++) {
-//                if(allUnique(str, i, j)) {
-//                    result = Math.max(result, j - i);
-//                }
-//            }
-//        }
-//        return result;
-//    }
-//
-//    public static boolean allUnique(String str, int start, int end) {
-//        Set<Character> set = new HashSet<>();
-//        for(int i = start; i < end; i++) {
-//            Character ch = str.charAt(i);
-//            if(set.contains(ch)) {
-//                return false;
-//            }
-//            set.add(ch);
-//        }
-//        return true;
-//    }
 
     /* Sliding-window solution
        O(n)
@@ -46,6 +19,7 @@ public class LongestSubstring {
         }
         return result;
     }
+
 
 
     public static void main(String[] args) {
