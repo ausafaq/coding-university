ba038f48a72daf4a310f79ac2990f8b16c7099c5
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2020-03-19 10:10:44 -0700

    Practice Questions

--- LeetCode/src/MaxAreaOfIsland.java
+++ LeetCode/src/MaxAreaOfIsland.java
@@ -1,17 +1,22 @@
 public class MaxAreaOfIsland {
 
     public static int maxAreaOfIsland(int[][] grid) {
+
+        // Check edge-cases: Null and Empty Grid
         if(grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
         int rows = grid.length;
         int cols = grid[0].length;
+
+        // Track visited islands
         boolean[][] visited = new boolean[rows][cols];
         int maxArea = 0;
+
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
                 if(grid[i][j] == 1 && !visited[i][j]) {
-                    int area = helper(grid, visited, i, j, rows, cols);
+                    int area = helper(grid, visited, i, j, rows, cols); // DFS Traversal
                     maxArea = Math.max(area, maxArea);
                 }
             }
@@ -21,21 +26,50 @@ public class MaxAreaOfIsland {
 
 
     private static int helper(int[][] grid, boolean[][] visited, int i, int j, int rows, int cols) {
-        if(i < 0 || i >= rows) {
+
+        // Check island indices are within the range
+        if(i < 0 || i >= rows || j < 0 || j >= cols) {
             return 0;
         }
-        if(j < 0 || j >= cols) {
-            return 0;
-        }
+
         if(visited[i][j]) {
             return 0;
         }
+
         if(grid[i][j] == 0) {
             return 0;
         }
+
+        visited[i][j] = true;
+
         return 1 + helper(grid, visited, i-1, j, rows, cols) +
                 helper(grid, visited, i+1, j, rows, cols) +
                 helper(grid, visited, i, j-1, rows, cols) +
                 helper(grid, visited, i, j+1, rows, cols);
+    }
+
+    public static void main(String[] args) {
+        int[][] testInput1 = {{0,0,1,0,0,0,0,1,0,0,0,0,0},
+                              {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                              {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                              {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                              {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                              {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                              {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                              {0,0,0,0,0,0,0,1,1,0,0,0,0}};
+        System.out.println("Max Area of Island: " +maxAreaOfIsland(testInput1)); // 6
+
+        int[][] testInput2 = {{1,1,0,0,0},
+                              {1,1,1,0,0},
+                              {0,0,0,1,1},
+                              {0,1,0,1,1}};
+        System.out.println("Max Area of Island: " +maxAreaOfIsland(testInput2)); // 5
+
+        int[][] testInput3 = {{0,0,0,0,0},
+                              {0,0,0,0,0}};
+        System.out.println("Max Area of Island: " +maxAreaOfIsland(testInput3)); // 0 for no island
+
+        System.out.println(maxAreaOfIsland(null)); // 0
+        System.out.println(maxAreaOfIsland(new int[][] {{}})); // 0 for empty grid
     }
 }
