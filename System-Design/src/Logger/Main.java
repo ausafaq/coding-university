88f48acb677668206b090befb25629969bac9253
ausafaq <ausaf.ahmed@hotmail.com>
2022-06-14 09:21:49 -0700

    Chain of responsibility pattern

---
+++ System-Design/src/Logger/Main.java
@@ -0,0 +1,28 @@
+package Logger;
+
+public class Main {
+
+    private static AbstractLogger getChainOfLoggers() {
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new FileLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        loggerChain.logMessage(AbstractLogger.INFO,
+                "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG,
+                "This is a debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR,
+                "This is a error information.");
+    }
+}
